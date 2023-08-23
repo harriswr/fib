cd4d11a6e7c39491ceb9e4d500698a7c1334fe57
William Harris <harriswr@mit.edu>
2023-08-23 10:47:37 -0400

    Updated fib.py to use cache. Updated meeting notes with more commands.

--- fib.py
+++ fib.py
@@ -1,10 +1,13 @@
 #!/home/harriswr/codes/anaconda3/bin/python
 
+cache = {0:0, 1:1}
+
 def fib(n):
-	if n == 0 or n == 1:
-		return n
-	else:
-		return fib(n-1) + fib(n-2)
+	if n in cache:
+		return cache[n]
+	res = fib(n-1) + fib(n-2)
+	cache[n] = res
+	return res
 
 if __name__ == "__main__":
-	print("The 20th Fibonacci Number is: ", fib(20))
+	print("The 20th Fibonacci number is: ", fib(20))
