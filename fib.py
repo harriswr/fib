3124308d699e5aeda06999f028ddc16114eb5ffe
harriswr <harriswr@mit.edu>
2023-08-23 10:35:53 -0400

    Adding Fibonacci code

---
+++ fib.py
@@ -0,0 +1,10 @@
+#!/home/harriswr/codes/anaconda3/bin/python
+
+def fib(n):
+	if n == 0 or n == 1:
+		return n
+	else:
+		return fib(n-1) + fib(n-2)
+
+if __name__ == "__main__":
+	print("The 20th Fibonacci Number is: ", fib(20))
